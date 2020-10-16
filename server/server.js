5cceeb88e3ca849407ed935aa41b2739e9632595
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-16 17:01:30 -0500

    tested front end connections, building out backend

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use( bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('server/public'));
+
+let toDoRouter = require('./routes/to_do_router');
+app.use('/currentList', toDoRouter);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log('up and running on port', PORT);
+})
