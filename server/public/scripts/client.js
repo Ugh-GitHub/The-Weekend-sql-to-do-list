8fdc1b6c38f96d4895f6a6bdea41f5075ba0aae3
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-18 21:07:02 -0500

    functioning get req pipeline

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,48 @@
+
+
 console.log("hello from js");
 
 $(document).ready(onReady);
 
 function onReady() {
     console.log("Hello from jquery");
+    getTaskList();
 }
+
+function getTaskList() {
+    $.ajax({
+        type: 'GET',
+        url: '/list'
+    }).then(function (response) {
+        console.log('Get got!');
+        $('#taskTBody').empty();
+        for (let i = 0; i < response.length; i++) {
+            if ( response[i].completed ) {
+                $('#taskTBody').append(`
+            <tr data-id=${response[i].id} data-completed=${response[i].completed}>
+                <td>${response[i].task}</td>
+                <td>Done!</td>
+                <td>${response[i].timestamp}</td>
+                <td><button class="changeStatus">Unmark</button></td>
+                <td><button class="deleteButton">Delete</button></td>   
+            </tr>`);
+            }
+            else {
+                $('#taskTBody').append(`
+            <tr data-id=${response[i].id} data-completed=${response[i].completed}>
+                <td>${response[i].task}</td>
+                <td>Incomplete</td>
+                <td>${response[i].timestamp}</td>
+                <td><button class="changeStatus">Mark as done</button></td>
+                <td><button class="deleteButton">Delete</button></td>   
+            </tr>`);
+            }
+            
+        }
+    });
+}
+
+
+   
+    
+  
--- server/routes/to_do_router.js
+++ server/routes/to_do_router.js
@@ -12,4 +12,51 @@ router.get('/', (req, res) => {
     });
 });
 
+router.put(`/completed/:id`, (req, res) => {
+    //console.log('in PUT request', req.body.direction, req.params.id);
+    // IF statement with parameter to undo completion
+    // if (req.body.status) {
+        let queryText = `UPDATE task_list
+        SET "timestamp" = CURRENT_TIMESTAMP, "completed?" = TRUE
+        WHERE id = $1;`
+    // }
+    // else {
+    // let queryText = `UPDATE task_list
+    //     SET "timestamp" = null, "completed?" = FALSE
+    //     WHERE id = $1;`
+    // }
+
+
+    
+    
+
+    
+    // THE NUMBER MATTERS, IT INDICATES WHICH PLACEHOLDER IS WHICH
+    
+    pool.query(queryText, [req.params.id]).then((result) => {
+        console.log(queryText);
+        // logs out ALL of the data from the database in the terminal (>.<)
+        //console.log('result from database', result);
+        res.sendStatus(200);
+    }).catch((error)=>{
+        console.log("error with get request",error);
+        res.sendStatus(500);
+    });
+});
+
+
+
+router.post('/', (req, res) => {
+    // req.body statements
+
+
+    let queryText = '';
+    pool.query(queryText).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log("error with request", error);
+        res.sendStatus(500);
+    });
+})
+
 module.exports = router;
