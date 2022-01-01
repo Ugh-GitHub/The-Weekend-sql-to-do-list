df703980c77429ce4fba3df7a71efb21666264c6
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2022-01-01 17:32:49 -0600

    replaced body-parser, updated port

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use( bodyParser.urlencoded({extended: true}));
+app.use( express.urlencoded({extended: true}));
 
 app.use(express.static('server/public'));
 
 let toDoRouter = require('./routes/to_do_router');
 app.use('/list', toDoRouter);
 
-const PORT = 5000;
+var port = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('up and running on port', PORT);
 });
